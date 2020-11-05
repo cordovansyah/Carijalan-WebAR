2c6621f45942aaa867feacbab71007cb5f95ef8f
Tech-5 <tech-5@Tech-5s-MacBook-Pro.local>
2020-11-06 02:16:44 +0700

    fix event.js

--- event.js
+++ event.js
@@ -1,26 +1,26 @@
-AFRAME.registerComponent('markerhandler', {
+// AFRAME.registerComponent('markerhandler', {
 
-    init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
+//     init: function() {
+//         const animatedMarker = document.querySelector("#animated-marker");
+//         const aEntity = document.querySelector("#animated-model");
 
-        // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                aEntity.setAttribute('scale', scale);
-            }
-        });
-}});
-
-// AFRAME.registerComponent('clickhandler', {
-//     //Init Function
-//     init: function(){
-//         //Cursor Component
-//         this.el.addEventListener('click', () => {
-//             window.location = 'https://github.com/cordovansyah/Carijalan-WebAR';
+//         // every click, we make our model grow in size :)
+//         animatedMarker.addEventListener('click', function(ev, target){
+//             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+//             if (aEntity && intersectedElement === aEntity) {
+//                 const scale = aEntity.getAttribute('scale');
+//                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+//                 aEntity.setAttribute('scale', scale);
+//             }
 //         });
-//     }
-// });
+// }});
+
+AFRAME.registerComponent('clickhandler', {
+    //Init Function
+    init: function(){
+        //Cursor Component
+        this.el.addEventListener('click', () => {
+            window.location = 'https://github.com/cordovansyah/Carijalan-WebAR';
+        });
+    }
+});
